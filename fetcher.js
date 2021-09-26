f5fd7565c4ff29c0856d6008cf4efd6fbf6fb238
Nick <nsagias@gmail.com>
2021-09-26 13:48:34 -0400

    Write file correctly

--- fetcher.js
+++ fetcher.js
@@ -20,7 +20,7 @@
 const request = require('request');
 const fs = require('fs')
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2); 
 // node fetcher.js http://www.example.edu/ ./index.html
 // console.log(args)
 // [ 'http://www.example.edu/', './index.html' ]
@@ -30,13 +30,11 @@ console.log(args[0]);
 // const address = args[0];
 const address = 'http://www.example.edu/';
 // const address = 'http://www.example.com/';
-const newFileName = args[1];
+
+// const newFileName = args[1];
+const newFileName = 'test.txt';
 console.log(newFileName)
-const content = `Some content!${new Date}()`;
-const geDir =  './';
-
-
-
+// const content = `Some content!${new Date}()`;
 
 
 
@@ -51,38 +49,33 @@ request(address, (error, response, body) => {
      `Status code is not 200 Received status code ${response.statusCode} exiting`);
    process.exit(1);
  }
-  console.log(response.statusCode) 
+
+  // edge case stuff
+  // try {
+  //   const stats = fs.statSync('./node_modules')
+  //   console.log(
+  //     stats.isFile(), //true
+  //     stats.isDirectory(), //false
+  //     stats.size
+  //   )
+  // } catch (err) {
+  //   console.error(err)
+  // }
+  // console.log(response.statusCode) 
   // console.log('body:', body); // Print the HTML for the Google homepage.
-  // if ()
-  // fs.writeFile(newFileName, content, err => {
-  //   if (err) {
-  //     console.error(err);
-  //     return;
-  //   }
-  //   // file written successfully
-  //   // if completed do this
-  //   // console.log(`Downloaded and saved ${1235} bytes to ${newFileName}.`
-
-  // })
-});
-
-
-
-// edge case stuff
-// try {
-//   const stats = fs.statSync('./node_modules')
-//   console.log(
-//     stats.isFile(), //true
-//     stats.isDirectory(), //false
-//     stats.size
-//   )
-// } catch (err) {
-//   console.error(err)
-// }
+  // if () {
+    fs.writeFile(newFileName, body, err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const stats = fs.statSync(newFileName)
+      // file written successfully
+     
+      console.log(`Downloaded and saved ${stats.size} bytes to ${newFileName}.`)
+   
+    });
 
 
 
-
-
-
-
+})
