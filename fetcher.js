6257f4f02d6f41b546610b58d268e5d14faf2fd4
Nick <nsagias@gmail.com>
2021-09-26 13:18:30 -0400

    Add file writer code, backing up

--- fetcher.js
+++ fetcher.js
@@ -17,14 +17,60 @@
 // 1. the local file already exists? 
 // 2. the local file path given is invalid? 
 // 3. the URL results in an error or non-200 result?
-
-const arg = process.argvs.slice(2)
-
 const request = require('request');
-request('http://www.google.com', function (error, response, body) {
+const fs = require('fs')
+
+const args = process.argv.slice(2);
+// node fetcher.js http://www.example.edu/ ./index.html
+// console.log(args)
+// [ 'http://www.example.edu/', './index.html' ]
+console.log(args[0]);
+
+
+// const address = args[0];
+const newFileName = args[1];
+console.log(newFileName)
+const content = `Some content!${new Date}()`;
+const geDir =  './';
+
+
+
+
+
+
+request(address, (error, response, body) => {
   console.error('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
+  fs.writeFile(outputFilename, content, err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    // file written successfully
+    // if completed do this
+    // console.log(`Downloaded and saved ${1235} bytes to ${newFileName}.`
+
+  })
 });
+
+
+
+// edge case stuff
+// try {
+//   const stats = fs.statSync('./node_modules')
+//   console.log(
+//     stats.isFile(), //true
+//     stats.isDirectory(), //false
+//     stats.size
+//   )
+// } catch (err) {
+//   console.error(err)
+// }
+
+
+
+
+
 
 
